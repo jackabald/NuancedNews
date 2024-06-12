d09b3cd419e67a585bd5ae390cd30ce0cde58fc6
Archibald <jarchibald@aims.wisc.edu>
2024-06-12 12:48:59 -0500

    feat: profile page

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <nav className="navMenu">
-      <a href="#">Home</a>
-      <a href="#">Profile</a>
+      <a href="/">Home</a>
+      <a href="/profile">Profile</a>
       <a href="#">More</a>
       <a href="#">Why?</a>
       <div className="dot"></div>
