54eeff08ca7927b5388ac142f6e54bfd63230f17
jackabald <jwarchibald@wisc.edu>
2024-06-13 00:07:25 -0500

    adding rss feeds: npr, wash post, huff post, wash examiner

--- Backend/rss_parser.py
+++ Backend/rss_parser.py
@@ -33,13 +33,27 @@ def save_to_json(filepath: str, data: List[Dict]) -> None:
 
 if __name__ == "__main__":
     urls = [
-        "https://feeds.bbci.co.uk/news/world/rss.xml",
-        "http://rss.cnn.com/rss/edition_world.rss",
-        "https://feeds.foxnews.com/foxnews/latest",
+        
         "https://feeds.a.dj.com/rss/RSSWorldNews.xml",
+        "https://feeds.a.dj.com/rss/RSSOpinion.xml",
+        "https://rss.nytimes.com/services/xml/rss/nyt/World.xml",
+        "https://rss.nytimes.com/services/xml/rss/nyt/Politics.xml",
+        "http://rss.cnn.com/rss/edition_world.rss",
+        "http://rss.cnn.com/rss/cnn_allpolitics.rss",
+        "https://moxie.foxnews.com/google-publisher/world.xml",
+        "https://moxie.foxnews.com/google-publisher/politics.xml",
+        "https://nypost.com/world-news/feed/",
+        "https://nypost.com/politics/feed/",
+        "https://www.washingtonpost.com/arcio/rss/category/politics/?itid=lk_inline_manual_2",
+        "https://feeds.washingtonpost.com/rss/world?itid=lk_inline_manual_35",
+        # more feeds from other publications
+        "https://feeds.npr.org/1001/rss.xml"
+        "https://rss.app/feeds/adYxbhoFRWTTGEtI.xml",
+        "https://feeds.bbci.co.uk/news/world/rss.xml",
         "https://www.dailywire.com/feeds/rss.xml",
         "https://www.newyorker.com/feed/news",
         "https://thegrayzone.com/feed/",
+        "https://www.washingtonexaminer.com/section/news/feed",
     ]
     articles = parse_feeds(urls)
     save_to_json("rss_feed.json", articles)
