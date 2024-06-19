c15def3d9c94a3da06e038f8c66937cbaa3a427d
Archibald <jarchibald@aims.wisc.edu>
2024-06-19 14:08:01 -0500

    styling: added save button (no func), removed scrollbar, updated global styles

--- Frontend/src/components/Article.jsx
+++ Frontend/src/components/Article.jsx
@@ -1,15 +1,36 @@
-import React from "react";
-import "./Article.css"
+import React, { useEffect } from "react";
+import "./Article.css";
+import $ from "jquery";
 
+const Article = ({ title, description, link }) => {
+  useEffect(() => {
+    $(function() {
+      $('.pp-bookmark-btn').on('click', function() {
+        var btn = $(this);
 
-const Article = ({ title, description, link}) => {
+        if (btn.hasClass("active")) {
+          btn.removeClass("active");
+        } else {
+          btn.addClass("active");
+        }
+      });
+    });
+  }, []);
+
   return (
     <div className="article-container">
-        <div className="article">
-            <h3>{title}</h3>
-            <p>{description}</p>
-            <a href={link} target="_blank" rel="noopener noreferrer">Read more</a>
-        </div>
+      <div className="article">
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <a href={link} target="_blank" rel="noopener noreferrer">Read more</a>
+        <button
+          type="button"
+          className="pp-bookmark-btn btn btn-default btn-lg pull-right"
+          data-context="investor"
+          data-context-action="view"
+          data-context-id="7"
+        ></button>
+      </div>
     </div>
   );
 };
