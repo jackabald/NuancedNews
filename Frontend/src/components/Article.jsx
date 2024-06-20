0b3243f54b95f2fa842b0a95f78b64be8e824d91
jackabald <jwarchibald@wisc.edu>
2024-06-19 22:27:20 -0500

    bookmark button turns red when pressed

--- Frontend/src/components/Article.jsx
+++ Frontend/src/components/Article.jsx
@@ -4,17 +4,16 @@ import $ from "jquery";
 
 const Article = ({ title, description, link }) => {
   useEffect(() => {
-    $(function() {
-      $('.pp-bookmark-btn').on('click', function() {
-        var btn = $(this);
+    const handleClick = (event) => {
+      const btn = $(event.target);
+      btn.toggleClass("active");
+    };
 
-        if (btn.hasClass("active")) {
-          btn.removeClass("active");
-        } else {
-          btn.addClass("active");
-        }
-      });
-    });
+    $(".pp-bookmark-btn").on("click", handleClick);
+
+    return () => {
+      $(".pp-bookmark-btn").off("click", handleClick);
+    };
   }, []);
 
   return (
@@ -22,7 +21,9 @@ const Article = ({ title, description, link }) => {
       <div className="article">
         <h3>{title}</h3>
         <p>{description}</p>
-        <a href={link} target="_blank" rel="noopener noreferrer">Read more</a>
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          Read more
+        </a>
         <button
           type="button"
           className="pp-bookmark-btn btn btn-default btn-lg pull-right"
