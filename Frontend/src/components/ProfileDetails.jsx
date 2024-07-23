38e2a4ccfb30c2751a405837996cc44414f816c4
Archibald <jarchibald@aims.wisc.edu>
2024-07-23 13:40:35 -0500

    feat: add profile details page after logging in

---
+++ Frontend/src/components/ProfileDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ProfileDetails = () => {
+  return (
+    <div className="profile-details">
+      <h1>Your Profile</h1>
+      <p>Welcome to your personalized profile page!</p>
+      {/* Add more personalized content here */}
+    </div>
+  );
+};
+
+export default ProfileDetails;
