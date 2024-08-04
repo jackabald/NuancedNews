ffb15762f576884a989bf2d3e5a90f578723fe17
Ananya Verma <ananyaa2311@gmail.com>
2024-08-05 00:47:33 +0530

    feat: implement logout button

--- Frontend/src/contexts/AuthContext.jsx
+++ Frontend/src/contexts/AuthContext.jsx
@@ -18,7 +18,7 @@ export const AuthProvider = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ currentUser }}>
+    <AuthContext.Provider value={{ currentUser, auth }}>
       {children}
     </AuthContext.Provider>
   );
