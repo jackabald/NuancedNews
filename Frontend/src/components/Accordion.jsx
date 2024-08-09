283541c055364534274d252f66a77fe9d3320d32
jackabald <jwarchibald@wisc.edu>
2024-08-09 13:44:11 -0500

    updated news headers in accordion
    Signed-off-by: jackabald <jwarchibald@wisc.edu>

--- Frontend/src/components/Accordion.jsx
+++ Frontend/src/components/Accordion.jsx
@@ -11,7 +11,7 @@ const Accordion = ({ news }) => {
       <div className="accordion">
         {groupedNews["CNN"] && (
           <div className="CNN">
-            <h1>CNN</h1>
+            <h1 className = "newsHeader">CNN</h1>
             {groupedNews["CNN"].map((article, index) => (
               <Article key={index} {...article} />
             ))}
@@ -19,7 +19,7 @@ const Accordion = ({ news }) => {
         )}
         {groupedNews["NYT"] && (
           <div className = "NYT">
-            <h1>NYT</h1>
+            <h1 className = "newsHeader">NYT</h1>
             {groupedNews["NYT"].map((article, index) => (
               <Article key={index} {...article} />
             ))}
@@ -27,7 +27,7 @@ const Accordion = ({ news }) => {
         )}
         {groupedNews["Gaurdian"] && (
           <div className="Guardian">
-            <h1>The Gaurdian</h1>
+            <h1 className = "newsHeader">The Gaurdian</h1>
             {groupedNews["Gaurdian"].map((article, index) => (
               <Article key={index} {...article} />
             ))}
@@ -35,7 +35,7 @@ const Accordion = ({ news }) => {
         )}
         {groupedNews["WSJ"] && (
           <div className="WSJ">
-            <h1>WSJ</h1>
+            <h1 className = "newsHeader">WSJ</h1>
             {groupedNews["WSJ"].map((article, index) => (
               <Article key={index} {...article} />
             ))}
@@ -43,7 +43,7 @@ const Accordion = ({ news }) => {
         )}
         {groupedNews["NYPost"] && (
           <div className="NYPost">
-            <h1>NY Post</h1>
+            <h1 className = "newsHeader">NY Post</h1>
             {groupedNews["NYPost"].map((article, index) => (
               <Article key={index} {...article} />
             ))}
@@ -51,7 +51,7 @@ const Accordion = ({ news }) => {
         )}
         {groupedNews["FOX"] && (
           <div className="FOX">
-            <h1>FOX</h1>
+            <h1 className = "newsHeader">FOX</h1>
             {groupedNews["FOX"].map((article, index) => (
               <Article key={index} {...article} />
             ))}
