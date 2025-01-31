2e12998fc775b6611590e577c05d4f0446400941
jgafnea <84107636+jgafnea@users.noreply.github.com>
2025-01-31 00:28:50 -0600

    feat: run RSS parser in Flask startup

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,34 @@
 import json
 
-from article_summarise import scrape_and_summarize
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+
+# from article_summarise import scrape_and_summarize
+from rss_parser import main as rss_parser  # noqa: F401
 
 app = Flask(__name__)
 CORS(app)
 
 
-@app.route("/api/news", methods=["GET"])
-def get_news():
-    with open("rss_feed.json", "r") as file:
-        data = json.load(file)
-    return jsonify(data)
+with app.app_context():
+    """
+    Startup function to initialize the app.
+    """
+    # Note: this used to be called `before_first_request` but that was deprecated.
+    # https://flask.palletsprojects.com/en/2.0.x/api/#flask.Flask.before_first_request
+
+    # Example startup functions:
+    # print("Connecting to Firestore")
+    # db = firestore.Client()
+
+    print("Running RSS Parser")
+    rss_parser()
+
+# @app.route("/api/news", methods=["GET"])
+# def get_news():
+#     with open("rss_feed.json", "r") as file:
+#         data = json.load(file)
+#     return jsonify(data)
 
 
 # @app.route('api/save', methods['POST'])
@@ -25,12 +41,14 @@ def get_news():
 #     except Exception as e:
 #         return f"An Error Occurred {e}", 400
 
-@app.route('/summarize', methods=['POST'])
-def summarize():
-    data = request.json
-    url = data['url']
-    summary = scrape_and_summarize(url)
-    return jsonify({'summary': summary})
+
+# @app.route("/summarize", methods=["POST"])
+# def summarize():
+#     data = request.json
+#     url = data["url"]
+#     summary = scrape_and_summarize(url)
+#     return jsonify({"summary": summary})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
